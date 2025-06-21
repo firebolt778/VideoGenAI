6c18d4f15e3597652184ef332e158a425246ad19
firebolt778 <firebolt77800@gmail.com>
2025-06-21 23:45:29 +0900

    Update routing in App component to redirect from root path to /channels

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,8 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Redirect } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Dashboard from "@/pages/dashboard";
 import Channels from "@/pages/channels";
 import VideoTemplates from "@/pages/video-templates";
 import Thumbnails from "@/pages/thumbnails";
@@ -19,7 +18,8 @@ function Router() {
       <Sidebar />
       <main className="flex-1 overflow-y-auto">
         <Switch>
-          <Route path="/" component={Dashboard} />
+          {/* <Route path="/" component={Dashboard} /> */}
+          <Route path="/" component={() => <Redirect to="/channels" />} />
           <Route path="/channels" component={Channels} />
           <Route path="/video-templates" component={VideoTemplates} />
           <Route path="/thumbnails" component={Thumbnails} />
